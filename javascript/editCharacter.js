4f5981254c91a3af38232b879c869be1a04b68e1
Manon <manon.vdm@hotmail.com>
2023-07-03 13:46:21 +0200

    edit page working linked and finished

--- javascript/editCharacter.js
+++ javascript/editCharacter.js
@@ -1,4 +1,4 @@
-const idCharacter = (new URLSearchParams(window.location.search)).get('id'); //window.location.search = URL actuel de ma page
+/*const idCharacter = (new URLSearchParams(window.location.search)).get('id'); //window.location.search = URL actuel de ma page
 
 fetch("https://character-database.becode.xyz/characters/" + idCharacter)
 .then(response => response.json())
@@ -9,4 +9,51 @@ function editData(data){
     document.getElementById("username").value = data["name"];
     document.getElementById("passwordConfirmation").value = data["shortDescription"];
     document.getElementById("textarea").value = data["description"];
+}*/
+
+// Fetch Character Data
+const idCharacter = (new URLSearchParams(window.location.search)).get('id');
+fetch("https://character-database.becode.xyz/characters/" + idCharacter)
+  .then(response => response.json())
+  .then(data => {
+    populateForm(data);
+    setupFormSubmission(data);
+  });
+
+// Populate Form Fields
+function populateForm(data) {
+    console.log(data["name"]);
+    document.getElementById("username").value = data["name"];
+    document.getElementById("passwordConfirmation").value = data["shortDescription"];
+    document.getElementById("textarea").value = data["description"];
 }
+
+// Form Submission
+function setupFormSubmission(characterData) {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+
+    const updatedCharacterData = {
+      id: characterData.id,
+      name: formData.get("name"),
+      description: formData.get("description"),
+      shortDescription: formData.get("shortDescription"),
+      image: characterData.image, // Preserve the existing image or update it if needed
+    };
+
+    fetch("https://character-database.becode.xyz/characters/" + characterData.id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedCharacterData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert("Character data updated successfully!");
+      });
+  });
+}
+
