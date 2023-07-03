aa376d9a7a375eab31adbdfbe4623436def2b3f9
FLE0 <iliassino@live.be>
2023-07-03 11:00:33 +0200

    creation fini

--- javascript/creation.js
+++ javascript/creation.js
@@ -15,39 +15,34 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formData = new FormData(form);
 
-  
-  const $file = document.querySelector(".local");
-  $file.addEventListener("change", (event) => {
-    const selectedfile = event.target.files;
-    if (selectedfile.length > 0) {
-      const [imageFile] = selectedfile;
-      const fileReader = new FileReader();
-      fileReader.onload = () => {
-        const srcData = fileReader.result;
-        console.log('base64:', srcData)
-      };
-      fileReader.readAsDataURL(imageFile);
-    }
-  });
-  
-  
-  const characterData = {
-    name: formData.get("name"),
-    description: formData.get("description"),
-    shortDescription: formData.get("shortDescription"),
-    Image: $file,
-  };
+  const $file = document.getElementById("file-upload");
+  const selectedFile = $file.files[0];
 
-  fetch("https://character-database.becode.xyz/characters/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(characterData),
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    const characterId = data.id;
-    alert("ID du personnage : " + characterId);
-  });
+  if (selectedFile) {
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      const base64Data = fileReader.result;
+      const characterData = {
+        name: formData.get("name"),
+        description: formData.get("description"),
+        shortDescription: formData.get("shortDescription"),
+        image: base64Data.split(',')[1],
+      };
+
+      fetch("https://character-database.becode.xyz/characters/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(characterData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const characterId = data.id;
+          alert("ID du personnage : " + characterId);
+        });
+    };
+
+    fileReader.readAsDataURL(selectedFile);
+  }
 });
