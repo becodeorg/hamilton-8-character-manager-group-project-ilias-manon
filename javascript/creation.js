cc8e30119c7db43cd3e599f3dfae983eb4ca2935
FLE0 <iliassino@live.be>
2023-07-03 10:40:46 +0200

    ma

--- javascript/creation.js
+++ javascript/creation.js
@@ -11,27 +11,43 @@ form.addEventListener("submit", (event) => {
 });
 
 
-
 form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
 
-    event.preventDefault();
   
-    const formData = new FormData(form);
-  
-    fetch("https://character-database.becode.xyz/characters/", {
-  
-      method: "POST",
-  
-      description: formData[3],
-  
-      shortdescription: formData[2],
-  
-      name: formData[1],
-  
-    }).then((response) =>
-  
-      response.text().then((data) => console.log(data))
-  
-    );
-  
+  const $file = document.querySelector(".local");
+  $file.addEventListener("change", (event) => {
+    const selectedfile = event.target.files;
+    if (selectedfile.length > 0) {
+      const [imageFile] = selectedfile;
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        const srcData = fileReader.result;
+        console.log('base64:', srcData)
+      };
+      fileReader.readAsDataURL(imageFile);
+    }
   });
+  
+  
+  const characterData = {
+    name: formData.get("name"),
+    description: formData.get("description"),
+    shortDescription: formData.get("shortDescription"),
+    Image: $file,
+  };
+
+  fetch("https://character-database.becode.xyz/characters/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(characterData),
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    const characterId = data.id;
+    alert("ID du personnage : " + characterId);
+  });
+});
